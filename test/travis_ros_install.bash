524634a4d4bae5410b59da132439bdede032d775
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-02-11 18:54:08 +0000

    Add package list for install

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,5 +1,11 @@
 #!/bin/bash -xve
 
+#required packages
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
+
 #ros install
 cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
