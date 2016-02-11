20ba9c559981525ab8a8a02ce537a35081eee604
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-02-11 19:02:48 +0000

    Add package make script

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,11 +1,5 @@
 #!/bin/bash -xve
 
-#required packages
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
-
 #sync and make
 rsync -av ./ ~/catkin_ws/src/raspimouse_ros/
 cd ~/catkin_ws
