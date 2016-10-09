33eadc8c18d93ca9122b66286054266ab9b66d9a
Ryuichi Ueda <ueda@cit-ar-lab.tech>
2016-10-09 10:36:28 +0000

    Scutch the test code

--- test/travis_test.bash
+++ test/travis_test.bash
@@ -4,27 +4,28 @@ set -o pipefail
 
 roscore &
 sleep 5
-rosrun pimouse_ros buzzer.py 2> ./out &
+rosrun pimouse_ros buzzer.py 2> ./buzzer.err &
 sleep 5
 
 ###TEST1: existance of the node###
-rosnode list		|
-grep '^/buzzer$'
+rosnode list | grep '^/buzzer$'
 
 ###TEST2: existance of the topic ###
-rostopic list		|
-grep '^/buzzer$'
+rostopic list | grep '^/buzzer$'
 
-###TEST3: writing test to /dev/rtbuzzer0  ###
+###TEST3: writing test to device files  ###
 
-#3.1 when the file doesn't exist
+buz=/dev/rtbuzzer0
+##3.1 when any file doesn't exist
+# in this moment we assume that the device files don't exist
 rostopic pub -1 '/buzzer' std_msgs/UInt16 1000
-grep '[ERROR].*/dev/rtbuzzer0' ./out
+grep "[ERROR].*$buz" ./buzzer.err
 
-#3.2 when the file exists
-sudo touch /dev/rtbuzzer0
-sudo chmod 666 /dev/rtbuzzer0
+##3.2 when the file exists
+sudo touch $buz
+sudo chmod 666 $buz
+
 rostopic pub -1 '/buzzer' std_msgs/UInt16 1234
 sleep 5
-cat /dev/rtbuzzer0
-grep '^1234$' /dev/rtbuzzer0
+
+echo 1234 | diff - $buz
