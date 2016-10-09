233d8542ecd063baf46abfb6f048b365f7e552ca
Ryuichi Ueda <ueda@cit-ar-lab.tech>
2016-10-09 10:07:05 +0000

    Add test to node check

--- test/travis_test.bash
+++ test/travis_test.bash
@@ -4,9 +4,25 @@ set -o pipefail
 
 roscore &
 sleep 5
-rosrun pimouse_ros buzzer1.py &
+rosrun pimouse_ros buzzer.py 2> ./out &
 sleep 5
 
-###TEST1: nodes are running or not###
+###TEST1: existance of the node###
 rosnode list		|
 grep '^/buzzer$'
+
+###TEST2: existance of the topic ###
+rostopic list		|
+grep '^/buzzer$'
+
+###TEST3: writing test to /dev/rtbuzzer0  ###
+
+#3.1 when the file doesn't exist
+rostopic pub -1 '/buzzer' std_msgs/UInt16 1000
+grep -F '[ERROR].*/dev/rtbuzzer0' ./out
+
+#3.2 when the file exists
+touch /dev/rtbuzzer0
+chmod 666 /dev/rtbuzzer0
+rostopic pub -1 '/buzzer' std_msgs/UInt16 1234
+grep '^1234$' /dev/rtbuzzer0
