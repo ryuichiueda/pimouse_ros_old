16ee567259f14e04f2e66da2320f4516a7a92e9a
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-11-16 00:26:03 +0900

    Remove redundant description

--- scripts/buzzer.py
+++ scripts/buzzer.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
-from __future__ import print_function
 import rospy
 from std_msgs.msg import UInt16
 
 def recv_buzzer(data):
-	rospy.loginfo(data.data)
-	try:
-		with open("/dev/rtbuzzer0","w") as f:
-			print(str(data.data),file=f)
-	except:
-		rospy.logerr("can't write to /dev/rtbuzzer0")
-	
+    rospy.loginfo(data.data)
+    bfile = "/dev/rtbuzzer0"
+    try:
+        with open(bfile,"w") as f:
+            f.write(str(data.data) + "\n")                
+    except:
+        rospy.logerr("can't write to " + bfile)
 
 if __name__ == '__main__':
-	rospy.init_node('buzzer')
-	rospy.Subscriber("buzzer", UInt16, recv_buzzer)
-	rospy.spin()
+    rospy.init_node('buzzer')
+    rospy.Subscriber("buzzer", UInt16, recv_buzzer)
+    rospy.spin()
 
