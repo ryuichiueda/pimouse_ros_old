96ccf8da38b2cbea873b34fcd080878e8461ae90
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-11-17 21:24:16 +0900

    Fix a bug

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -7,6 +7,10 @@ from pimouse_ros.msg import LightSensorValues
 class LightsensorTest(unittest.TestCase):
     lf,ls,rs,rf = 0,0,0,0
     count = 0
+
+    def __init__(self):
+        pub = rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
         self.assertIn('/lightsensors',nodes, "node does not exist")
@@ -28,7 +32,6 @@ class LightsensorTest(unittest.TestCase):
         with open("/dev/rtlightsensor0","w") as f:
             f.write("-1 0 123 4321\n")
 
-        pub = rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
         time.sleep(3)
         self.check_values(4321,123,0,-1)
 
@@ -41,7 +44,6 @@ class LightsensorTest(unittest.TestCase):
         rospy.set_param('lightsensors_freq',10)
         time.sleep(2)
         
-
 if __name__ == '__main__':
     time.sleep(5)
     rospy.set_param('lightsensors_freq',10)
