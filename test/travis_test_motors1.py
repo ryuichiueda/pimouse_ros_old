1b12e537dbf15126bc31719891f2b0f140fab7e7
Ryuichi Ueda <ryuichiueda@gmail.fcom>
2016-11-20 07:55:14 +0900

    Add test for motor_raw

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -6,30 +6,9 @@ from pimouse_ros.msg import Motion
 from std_srvs.srv import Trigger, TriggerResponse
 
 class MotorTest(unittest.TestCase):
-    def setUp(self):
-        rospy.wait_for_service('/motor_on')
-        rospy.wait_for_service('/motor_off')
-
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
         self.assertIn('/motors', nodes, "node does not exist")
-
-    def test_on_off(self):
-        on = rospy.ServiceProxy('/motor_on', Trigger)
-        ret = on()
-        self.assertEqual(ret.success, True, "motor on does not succeeded")
-        self.assertEqual(ret.message, "ON", "motor on wrong message")
-        with open("/dev/rtmotoren0","r") as f:
-            data = f.readline()
-            self.assertEqual(data,"1\n","wrong value in rtmotor0 at motor on")
-
-        off = rospy.ServiceProxy('/motor_off', Trigger)
-        ret = off()
-        self.assertEqual(ret.success, True, "motor off does not succeeded")
-        self.assertEqual(ret.message, "OFF", "motor off wrong message")
-        with open("/dev/rtmotoren0","r") as f:
-            data = f.readline()
-            self.assertEqual(data,"0\n","wrong value in rtmotor0 at motor off")
 
     def test_put_value(self):
         pub = rospy.Publisher('/motion', Motion)
@@ -48,5 +27,6 @@ class MotorTest(unittest.TestCase):
             self.assertEqual(data,"456\n","value does not written to rtmotor_raw_r0")
 
 if __name__ == '__main__':
+    time.sleep(3)
     rospy.init_node('travis_test_motors')
     rostest.rosrun('pimouse_ros','travis_test_motors', MotorTest)
