54a2cd7793975568f136fc4eb3d796cc36701d7a
Ryuichi Ueda <ueda@cit-ar-lab.tech>
2016-12-08 11:17:18 +0000

    Add variables in the type

--- scripts/lightsensors.py
+++ scripts/lightsensors.py
@@ -27,11 +27,14 @@ if __name__ == '__main__':
         try:
             with open(devfile,'r') as f:
                 data = f.readline().split()
+                data = [ int(e) for e in data ] 
                 d = LightSensorValues()
-                d.right_forward = int(data[0])
-                d.right_side = int(data[1])
-                d.left_side = int(data[2])
-                d.left_forward = int(data[3])
+                d.right_forward = data[0]
+                d.right_side = data[1]
+                d.left_side = data[2]
+                d.left_forward = data[3]
+                d.sum_all = sum(data)
+                d.sum_forward = data[0] + data[3]
                 pub.publish(d)
         except:
             rospy.logerr("cannot open " + devfile)
