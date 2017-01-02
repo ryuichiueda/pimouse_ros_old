7ec28dfc8ccb0be667a7926e8a4de3d625e8e973
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-01-02 10:15:53 +0900

    Arrange codes

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -34,9 +34,9 @@ class BuzzerTest(unittest.TestCase):
         self.client.wait_for_server()
         self.client.send_goal(goal,feedback_cb=self.feedback_cb)
         self.client.wait_for_result()
-        self.assertTrue(self.client.get_result(),"invalid result")
 
         time.sleep(3)
+        self.assertTrue(self.client.get_result(),"invalid result")
         self.assertEqual(goal.freqs,self.device_values,"invalid feedback:" 
                 + ",".join([str(e) for e in self.device_values]))
 
@@ -45,6 +45,7 @@ class BuzzerTest(unittest.TestCase):
         self.client.send_goal(goal,feedback_cb=self.feedback_cb)
         self.client.wait_for_result(rospy.Duration.from_sec(0.5))
 
+        time.sleep(3)
         self.assertFalse(self.client.get_result(),"stop is requested but return true")
         self.assertFalse(goal.freqs == self.device_values,"not stopped")
 
